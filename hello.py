2b165a3fabdd5e5e3716235d481466c12e30a289
atharv138 <suryavanshiathrav072@gmail.com>
2025-07-13 19:46:41 +0530

    Commit

--- hello.py
+++ hello.py
@@ -1,1 +1,29 @@
-print("Hello World!\n")
+print("Hello World!")
+print("My name is Atharv Milind Suryavanshi.","I am from kururndwad.")
+print("The number is:",23)
+print("The sum of 12 and 34 is:",12+34)
+
+# Use of integer variables
+num1=12
+num2=23
+
+print("The sum of num1=12 and num2=23 is:",num1+num2)
+
+# Use of the string and float variable
+
+price=923.23
+name="Atharv Milind Suryavanshi"
+
+print("Price=",price)
+print("Name=",name)
+
+
+var=None
+bool=True
+
+# Types of the variables used 
+print("Type of variable num1:",type(num1))
+print("Type of variable price:",type(price))
+print("Type of variable name:",type(name))
+print("Type of variable bool:",type(bool))
+print("Type of variable none:",type(var))

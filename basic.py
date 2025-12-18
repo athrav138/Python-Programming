01ad3d43454dc9b1f1258061781b3bb686b20cfc
atharv138 <suryavanshiathrav072@gmail.com>
2025-12-18 11:32:12 +0530

    co

--- basic.py
+++ basic.py
@@ -4,5 +4,19 @@ y = 1
 def add(a,b):
   return a+b
 
-print(add(x,y))
+def subtract(a,b):
+  return a-b
+
+def multiply(a,b):
+  return a*b
+
+def division(a,b):
+  return a/b
+
+print("Addition:",add(x,y))
+print("Subtraction:",subtract(x,y))
+print("Multiplication:",multiply(x,y))
+print("Division:",division(x,y))
+
+
 

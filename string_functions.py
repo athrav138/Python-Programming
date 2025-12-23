e9343792f8d10c9c45325c246fa5059f95c6528e
atharv138 <suryavanshiathrav072@gmail.com>
2025-12-23 17:16:00 +0530

    d

---
+++ string_functions.py
@@ -0,0 +1,9 @@
+#THese are the working of the different different string funcitons
+
+string = "my name is Atharv"
+
+# Its check the given string is ends with the given string or not
+# Its a case sensitive function
+
+print("Ends with: ",string.endswith('atharv'))
+

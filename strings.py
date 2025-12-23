86db0cfabe19d2f4e4a20a568591c4e47e71304e
atharv138 <suryavanshiathrav072@gmail.com>
2025-12-23 17:11:51 +0530

    String Operations

--- strings.py
+++ strings.py
@@ -8,7 +8,7 @@ full_name = first_name + middle_name + last_name
 print("Full name: ",full_name)
 print("No. of Characters in name: ",len(full_name)-2)
 
-# slicing a string
+# slicing a  string
 slice = first_name[0:3]
 slice1 = first_name[:3]
 slice2 = first_name[0:]

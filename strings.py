50d5be333516648ace3fc73a800f82b8651614c8
atharv138 <suryavanshiathrav072@gmail.com>
2025-12-23 16:52:04 +0530

    String

--- strings.py
+++ strings.py
@@ -1,10 +1,14 @@
 
-fist_name = "Atharv "
+first_name = "Atharv "
 middle_name = "Milind "
 last_name = "Suryavanshi"
 
-full_name = fist_name + middle_name + last_name
+full_name = first_name + middle_name + last_name
 
 print("Full name: ",full_name)
 print("No. of Characters in name: ",len(full_name)-2)
+
+# slicing a string
+slice = first_name[0:3]
+print(slice)
 

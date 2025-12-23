b4d6e0b9ccd9abfd3f5337d0c84d57d2c3a32ce4
atharv138 <suryavanshiathrav072@gmail.com>
2025-12-23 15:32:46 +0530

    Strings

---
+++ strings.py
@@ -0,0 +1,10 @@
+
+fist_name = "Atharv "
+middle_name = "Milind "
+last_name = "Suryavanshi"
+
+full_name = fist_name + middle_name + last_name
+
+print("Full name: ",full_name)
+print("No. of Characters in name: ",len(full_name)-2)
+

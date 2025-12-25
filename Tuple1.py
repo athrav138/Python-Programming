bcb6137c89de8ae76fe9b1f396c4dcbd8800c616
atharv138 <suryavanshiathrav072@gmail.com>
2025-12-25 14:49:05 +0530

    Tuples and list Practice combined

---
+++ Tuple1.py
@@ -0,0 +1,15 @@
+
+grade = ('C','D','A','A','B','B','A')
+
+print("The students having grade 'a' is: ",grade.count('A'))
+print(grade)
+
+
+List_grade = []
+
+for i in grade :
+  List_grade.append(i)
+
+List_grade.sort()
+print(List_grade)
+

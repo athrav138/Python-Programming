e1d48b31c3813206fab5ebb1ed99a72928dacfe0
atharv138 <suryavanshiathrav072@gmail.com>
2025-12-26 17:40:21 +0530

    count number of vowels in the string

---
+++ count_vowels.py
@@ -0,0 +1,14 @@
+def count_vowels(str):
+  str = str.lower()
+  count = 0
+  
+  for i in range(len(str)):
+    if str[i]=='a' or str[i]=='e' or str[i]=='i' or str[i]=='o' or str[i]=='u' :
+      count += 1
+
+  return count
+
+
+string = "Alaioue"
+
+print("The number of vowels in {string} is: ",count_vowels(string))

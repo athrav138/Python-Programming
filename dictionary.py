dbe8621fa6e6099113d0e035c19d747b266570ac
atharv138 <suryavanshiathrav072@gmail.com>
2025-12-27 15:09:12 +0530

    Dictionary

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+
+student_marks = {
+  "Atharv" : 98,
+  "Rucha" : 82,
+  "Sujata" : 63,
+  "Milind" : 55
+}
+
+print(student_marks)
+print(student_marks.items())
+#print(student_marks("Atharv"))
+print(student_marks.get("Rucha"))

325637c278d4d1dced872a326de34f18c66f80e6
atharv138 <suryavanshiathrav072@gmail.com>
2025-12-27 16:36:31 +0530

    Dictionary of student marks

--- dictionary.py
+++ dictionary.py
@@ -28,7 +28,9 @@ print(student_marks)
 # to print the all items in the dictionary 
 student_marks.values()
 
+# to print the size of dictionary in the bytes\
 print("The size of the dictionary is: ",student_marks.__sizeof__())
+
 
 # deletes the all the data int the dictionary 
 student_marks.clear()

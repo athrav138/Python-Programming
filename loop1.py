82be9252c8fb670a06191092dfe3672940f18475
atharv138 <suryavanshiathrav072@gmail.com>
2025-12-28 12:31:07 +0530

    loops

---
+++ loop1.py
@@ -0,0 +1,20 @@
+list = []
+
+for i in range (1,11):
+  print(i*i)
+  list.append(i*i)
+
+for i in range(len(list)):
+  print(list[i])
+
+
+tuple = (1,2,3,4,564,23,5,-23,4522,5)
+
+
+# Using lenear search
+key = 5
+i = 0
+while key!=tuple[i]:
+  i+=1
+else :
+ print(f"{key} is the key found in tuple at {i+1} postion")

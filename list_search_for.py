0cf1dcc1580ef259d692deb23635b5120a04ecc5
atharv138 <suryavanshiathrav072@gmail.com>
2025-12-28 12:41:47 +0530

    for

---
+++ list_search_for.py
@@ -0,0 +1,18 @@
+list = [1,2,3,5442,35,33649,9,456668,3456]
+
+for i in list :
+  print(i)
+
+key = 9
+tuple = range(10)
+idx =0 
+for i in tuple :
+  if(i==key) :
+    print(f"{key} key is found {idx+1} postion")
+  idx+=1
+
+for i in range(100,0,-1):
+  print(i)
+
+for i in range(10):
+  pass # this is keyword in the python used to stay the staments of loops as null

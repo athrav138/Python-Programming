22412becaa4a24b7e1ea4e5a21b6225a0ec56397
atharv138 <suryavanshiathrav072@gmail.com>
2025-12-28 12:50:14 +0530

    fact and sum using for and while loop

---
+++ fact_for.py
@@ -0,0 +1,7 @@
+n = int(input("Enter the number upto factorial want: "))
+
+fact = 1
+
+for i in range(1,n+1):
+  fact *= i
+  print(fact)
---
+++ sum_n_while.py
@@ -0,0 +1,9 @@
+n = int(input("Enter the number upto sum want: "))
+
+i = 1
+sum = 0
+while i<=n :
+  sum += i
+  i+=1
+
+print("The sum of 1 to n is number is: ",sum)

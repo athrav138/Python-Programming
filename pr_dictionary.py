4eaacad2734047c42197a29da0dcfee0cdfb0c34
atharv138 <suryavanshiathrav072@gmail.com>
2025-12-28 11:25:25 +0530

    Practice of Sets and dicitoanry

---
+++ pr_dictionary.py
@@ -0,0 +1,11 @@
+# Practice of Sets and dicitoanry
+dictionary = {
+  "table" : ("a peice of furniture", "list of facts and figures"),
+  "cat" : "a small animal"
+}
+
+print(dictionary)
+
+stud_sub = {"python","java","c++","python","js","java","python","c++","c"}
+
+print("The total number of classrooms requried is",len(stud_sub))

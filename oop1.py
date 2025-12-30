45d68122bee5b9a96a8b8c137ee17ea370cd3564
atharv138 <suryavanshiathrav072@gmail.com>
2025-12-30 22:27:27 +0530

    commit

---
+++ oop1.py
@@ -0,0 +1,12 @@
+class Student:
+  name = "Atharv Milind Suryavanshi"
+
+  def __init__(self,name):
+    self.name = name
+
+  def display(self):
+    print(self.name)
+
+
+s1 = Student("Atharv Milind Suryavanshi")
+s1.display()

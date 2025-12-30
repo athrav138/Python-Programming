0ea02d03663e711f9be213cff7b982a7b6c5451c
atharv138 <suryavanshiathrav072@gmail.com>
2025-12-30 20:47:13 +0530

    commit

--- file_pr3.py
+++ file_pr3.py
@@ -2,7 +2,10 @@ with open("practice.txt","r") as f :
   data = f.read()
   data1 = data.split(',')
   print(data1)
+  count = 0
   for i in range(len(data1)):
       data1[i] = int(data1[i])
-  print(data1)
-  #print(count)
+      if(data1[i]%2 == 0):
+        count += 1    
+
+  print("The count of even numbers in file is: ",count)

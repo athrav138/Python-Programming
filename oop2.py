c2a91f4d3e64d9aa0b44b40301d306fd4a3cd9b1
atharv138 <suryavanshiathrav072@gmail.com>
2025-12-31 20:18:59 +0530

    commit

--- oop2.py
+++ oop2.py
@@ -1,7 +1,7 @@
 class Student :
   def __init__(self,name,m1,m2,m3):
     self.name = name
-    
+
     self.m1 = m1
     self.m2 = m2
     self.m3 = m3
@@ -12,7 +12,7 @@ class Student :
 
   def display(self):
     print(f"The information of student {self.name} is :")
-    print(f"The average of marks is: ",(self.m1 + self.m2 + self.m3)/3)
+    print(f"The average   of marks is: ",(self.m1 + self.m2 + self.m3)/3)
 
 s1 = Student("Atharv",99,99,99)
 s1.greet(s1.name)

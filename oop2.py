21e4bc41383f05c90f3855f2cd3c2d4f97b9c931
atharv138 <suryavanshiathrav072@gmail.com>
2025-12-31 20:17:58 +0530

    static method

---
+++ oop2.py
@@ -0,0 +1,20 @@
+class Student :
+  def __init__(self,name,m1,m2,m3):
+    self.name = name
+    self.m1 = m1
+    self.m2 = m2
+    self.m3 = m3
+
+  @staticmethod
+  def greet(name):
+    print("Hello,",name)
+
+  def display(self):
+    print(f"The information of student {self.name} is :")
+    print(f"The average of marks is: ",(self.m1 + self.m2 + self.m3)/3)
+
+s1 = Student("Atharv",99,99,99)
+s1.greet(s1.name)
+s1.display()
+  
+  

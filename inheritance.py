e850fe3d86d0b84d1604759074e13a1ec141dae9
atharv138 <suryavanshiathrav072@gmail.com>
2026-01-02 20:27:46 +0530

    com

--- inheritance.py
+++ inheritance.py
@@ -1,2 +1,18 @@
-class Toyata:
+class Car:
+  def get(name,n_seats):
+    car_name = name
+    number_of_seats = n_seats
+
+class ToyotaCar(Car):
   
+  def __init__(self,c):
+    color = c
+
+  def display():
+    print(car_name)
+    print(number_of_seats)
+    print(color)
+
+t1 = ToyotaCar("BLACK")
+t1.get("TOYOTA FOrtuner",5)
+t1.display()

3c8f964d83bbdad48921e66a678c947b603eb231
atharv138 <suryavanshiathrav072@gmail.com>
2026-01-02 20:45:54 +0530

    multiple inheritance

---
+++ multiple_inheritance.py
@@ -0,0 +1,25 @@
+class Car:
+  def get(self, name, n_seats):
+    self.car_name = name
+    self.number_of_seats = n_seats
+
+
+class ToyotaCar:
+  def __init__(self, c):
+    self.color = c
+
+
+class Fortuner(Car, ToyotaCar):
+
+  def __init__(self, c):
+    ToyotaCar.__init__(self, c)
+
+  def display(self):
+    print(self.car_name)
+    print(self.number_of_seats)
+    print(self.color)
+
+
+t1 = Fortuner("BLACK")
+t1.get("TOYOTA Fortuner", 5)
+t1.display()

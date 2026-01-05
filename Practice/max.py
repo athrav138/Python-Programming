e17d6f3980a8ee14119d23a5b50fbae5466ccca0
atharv138 <suryavanshiathrav072@gmail.com>
2026-01-05 22:09:53 +0530

    max function

---
+++ Practice/max.py
@@ -0,0 +1,20 @@
+arr = [1,2,56,65,4,2,3,4]
+
+print(max(arr))
+print(min(arr))
+print(arr.append(12))
+print(arr)
+print(insert(2,"hellow"))
+"""append
+extend
+insert
+remove
+pop
+clear
+index
+count
+sort
+reverse
+copy
+
+"""

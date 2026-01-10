a8d3b5be490f6b6e06a924fd5609cb9949117059
atharv138 <suryavanshiathrav072@gmail.com>
2026-01-10 18:38:47 +0530

    operators

---
+++ Practice/pr3_identity.py
@@ -0,0 +1,10 @@
+a = 5
+b = a
+
+x = 5
+y = 5
+
+print("Identity using 'is': ",a is b)
+print("Identity using 'is': ",x is y)
+print("Identity using 'is not': ",x is not y) 
+print("Identity using 'is not': ",a is not b) 
---
+++ Practice/pr3_logical.py
@@ -0,0 +1,10 @@
+n1 = int(input("Enter number 1 :"))
+n2 = int(input("Enter number 2:"))
+
+             
+print(f"And of n1={n1} and n2={n2} is: ",n1 and n2)
+
+print(f"Or of n1={n1} and n2={n2} is: ",n1 or n2)
+
+print(f"Not of n1={n1} and n2={n2} is: ",not n1)
+    
